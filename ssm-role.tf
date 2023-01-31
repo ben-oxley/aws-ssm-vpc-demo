30ec94cc7affce4a9ac9c57d718233ab4383ba25
Ben Oxley <ben.oxley@gmail.com>
2023-01-31 21:22:07 +0000

    Updated formatting

--- ssm-role.tf
+++ ssm-role.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_instance_profile" "dev-resources-iam-profile" {
-name = "ec2_profile"
-role = aws_iam_role.dev-resources-iam-role.name
+  name = "ec2_profile"
+  role = aws_iam_role.dev-resources-iam-role.name
 }
 resource "aws_iam_role" "dev-resources-iam-role" {
-name        = "dev-ssm-role"
-description = "The role for the developer resources EC2"
-assume_role_policy = <<EOF
+  name               = "dev-ssm-role"
+  description        = "The role for the developer resources EC2"
+  assume_role_policy = <<EOF
 {
 "Version": "2012-10-17",
 "Statement": {
@@ -15,11 +15,11 @@ assume_role_policy = <<EOF
 }
 }
 EOF
-tags = {
-stack = "test"
-}
+  tags = {
+    stack = "test"
+  }
 }
 resource "aws_iam_role_policy_attachment" "dev-resources-ssm-policy" {
-role       = aws_iam_role.dev-resources-iam-role.name
-policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.dev-resources-iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
